fd9454f29a2f202636d21aa8599864c21bdcdc76
Pahul <52893813+pahuldeep@users.noreply.github.com>
2023-11-19 00:16:36 +0530

    Add files via upload

---
+++ hello.cu
@@ -0,0 +1,12 @@
+#include "hello.h"
+
+__global__ void hello(){
+    printf("GPU:: Hello World\n");
+}
+
+void show_hello(void){
+
+    hello<<<1,10>>>();
+    cudaDeviceSynchronize();
+    printf("DONE\n");
+}
